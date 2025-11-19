5ae25463c1128205232b6e0b6e755311b74cbfcc
Kerri Jensen <kerrijensen13@gmail.com>
2025-11-18 17:49:12 -0700

    Fix shops page and home page templates

--- shops/urls.py
+++ shops/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import list_shops, shop_detail, shop_menu
+
 urlpatterns = [
-    path("", list_shops),                 # GET /api/shops
-    path("<int:pk>", shop_detail),        # GET /api/shops/:id
-    path("<int:pk>/menu", shop_menu),     # GET /api/shops/:id/menu
+    path("", list_shops, name = "list_shops"),                 # GET /api/shops
+    path("<int:pk>", shop_detail, name = "shop_detail"),        # GET /api/shops/:id
+    path("<int:pk>/menu", shop_menu, name = "shop_menu"),     # GET /api/shops/:id/menu
 ]
